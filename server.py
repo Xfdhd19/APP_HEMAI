ce1b20c271655050b6ba04830de756e8cb91496e
Xfdhd19 <miggytuazon19@gmail.com>
2025-04-03 12:30:37 +0800

    Update server.py

--- server.py
+++ server.py
@@ -3,10 +3,10 @@ import PIL.Image
 from ultralytics import YOLO
 
 # Load YOLOv8 model
-model = YOLO(r"C:\Users\xfdhd\Downloads\train3-20250308T154830Z-001\train3\weights\best.pt") # Replace with your model path
+model = YOLO(r"weights/best.pt") # Replace with your model path
 
 # Streamlit app
-st.image(r"C:\Users\xfdhd\Downloads\LOGO_HEMAI.png", use_column_width=True)
+st.image(r"assets/LOGO_HEMAI.png", use_column_width=True)
 
 st.title("Anemia Type Classifier")
 st.markdown(
