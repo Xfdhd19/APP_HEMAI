831a6eaae6d931dcdcc49db298ddc67a16297768
Xfdhd19 <miggytuazon19@gmail.com>
2025-04-03 20:52:24 +0800

    Update server.py

--- server.py
+++ server.py
@@ -37,7 +37,7 @@ st.markdown(
 )
 
 # Upload image
-uploaded_image = st.file_uploader("📷Please upload an image of a blood smear from a microscope view.", type=["jpg", "png", "jpeg"])
+uploaded_image = st.file_uploader("📷 Please upload an image of a blood smear from a microscope view.", type=["jpg", "png", "jpeg"])
 
 if uploaded_image is not None:
     # Read image
@@ -47,5 +47,5 @@ if uploaded_image is not None:
     results = model(image)
 
     # Display results
-    st.image(results[0].plot(), caption="Detected Objects", use_column_width=True)
+    st.image(results[0].plot(), caption="Detected Objects", use_container_width=True)
    
